e2ca1e63a60b5ec4a5734eb056433918c6f7367e
phiwe saba <sabasbulele81@gmail.com>
2023-03-28 20:58:17 +0200

    added JavaScript to the application form.

--- js/script.js
+++ js/script.js
@@ -11,4 +11,28 @@ const navToggler = document.querySelector(".nav-toggler");
     else{
     	nav.removeAttribute("style");
     }
- } 
+} 
+
+const form = document.getElementById('appointment-form');
+const nameInput = document.getElementById('name');
+const lastInput = document.getElementById('last_name');
+const emailInput = document.getElementById('email');
+const phoneInput = document.getElementById('file');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Validate form data
+  if (nameInput.value === '' || emailInput.value === '' || phoneInput.value === '' || dateInput.value === '' || timeInput.value === '' || reasonInput.value === '') {
+    alert('Please fill in all fields.');
+    return false;
+}
+
+// Submit form data
+const formData = new FormData(form);
+const xhr = new XMLHttpRequest();
+xhr.open('POST', 'your-server-url-here');
+xhr.send(formData);
+
+alert
+})
